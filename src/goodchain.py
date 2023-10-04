8e16427d8d02d391bb0939b044cbf3ec9ce721a0
joelscholtz <joelscholtz@hotmail.com>
2023-10-04 10:48:58 +0200

    Improved the meny for future additions

--- src/goodchain.py
+++ src/goodchain.py
@@ -4,41 +4,44 @@ from utils import print_header
 
 current_user = None
 
+def display_menu(is_logged_in):
+    if is_logged_in:
+        return [
+            {"option": "1", "text": "Logout", "action": lambda: logout_and_clear_user()},
+            {"option": "2", "text": "Exit", "action": lambda: "exit"}
+        ]
+    else:
+        return [
+            {"option": "1", "text": "Register", "action": lambda: register_user()},
+            {"option": "2", "text": "Login", "action": lambda: login_and_set_user()},
+            {"option": "3", "text": "Exit", "action": lambda: "exit"}
+        ]
+    
+def logout_and_clear_user():
+    global current_user
+    logout_user()
+    current_user = None
+
+def login_and_set_user():
+    global current_user
+    current_user = login_user()
+    return current_user
+
 def main_menu():
     global current_user
     print_header()
     while True:
-        if current_user:
-            print("┌───────────────┐")
-            print("│ 1. Logout     │")
-            print("│ 2. Exit       │")
-            print("└───────────────┘")
-        else:
-            print("┌───────────────┐")
-            print("│ 1. Register   │")
-            print("│ 2. Login      │")
-            print("│ 3. Exit       │")
-            print("└───────────────┘")
+        options = display_menu(current_user)
+        for opt in options: 
+            print(f"| {opt['option']}. {opt['text']} |")
         choice = input('> ')
-        if not current_user:
-            if choice == '1':
-                register_user()
-            elif choice == '2':
-                current_user = login_user()
-            elif choice == '3':
-                break
-            else:
-                print_header()
-                print('Invalid choice')
-        else:
-            if choice == '1':
-                logout_user()
-                current_user = None
-            elif choice == '2':
-                break
-            else:
-                print_header(current_user)
-                print('Invalid choice')
+
+        action = next((opt['action'] for opt in options if opt['option'] == choice), None)
+        if action and action() == "exit":
+            break
+        elif not action:
+            print_header(current_user)
+            print('Invalid choice')
 
 setup_db()
 
