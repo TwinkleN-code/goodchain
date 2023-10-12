b3800c2811af3d534e2d017e64419e213bec4f9a
joelscholtz <joelscholtz@hotmail.com>
2023-10-12 08:16:18 +0200

    added key.txt to gitignore

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def print_header(username=None):
-    # clear_screen()
+    clear_screen()
     header =("##################")
     title = ("#   GOODCHAIN    #")
 
