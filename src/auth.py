0d6c16743143c347b2446cf643af9e55b9126449
joelscholtz <joelscholtz@hotmail.com>
2023-10-13 02:23:28 +0200

    Removed unneccesary imports

--- src/auth.py
+++ src/auth.py
@@ -6,13 +6,8 @@ from keys import *
 from utils import print_header
 from database import Database
 from transaction import transaction_pool, Transaction, REWARD
-from storage import save_to_file, load_from_file
-# from cryptography.hazmat.primitives import hashes
-# from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-# from cryptography.hazmat.backends import default_backend
-# from base64 import urlsafe_b64encode, urlsafe_b64decode
+from storage import load_from_file
 import hashlib
-import os
 
 PEPPER = b"MySecretPepperValue"
 
